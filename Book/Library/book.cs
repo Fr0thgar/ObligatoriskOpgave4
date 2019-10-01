562c1e815c565a5fbe5fef8ac58c1150ea11ae10
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2019-10-01 14:49:47 +0200

    all of it

---
+++ Book/Library/book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books.Library
+{
+    public class Book
+    {
+        private string _title; // minimum 2 tegn
+        private string _author;
+        private int _pages;
+        private string _isbn;
+
+
+        public Book(string title, string author, int pages, string isbn)
+        {
+            _title = title;
+            _author = author;
+            _pages = pages;
+            _isbn = isbn;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Author
+        {
+            get => _author;
+            set => _author = value;
+        }
+
+        public int Pages
+        {
+            get => _pages;
+            set => _pages = value;
+        }
+
+        public string Isbn
+        {
+            get => _isbn;
+            set => _isbn = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Title)}: {Title}, {nameof(Author)}: {Author}, {nameof(Pages)}: {Pages}, {nameof(Isbn)}: {Isbn}";
+        }
+        public Book()
+        {
+
+        }
+    }
+}
---
+++ ObligatoriskOpgave4/Controllers/BookController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Books.Library;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ObligatoriskOpgave4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> data = new List<Book>()
+        {
+            new Book("Scoops, a guide","Aaron B. Carlson", 200, "HDLEKNSIBN218"),
+            new Book("Shrubberies", "The Knight Who Say Ni", 3000, "LOKNGTREDF852"),
+            new Book("The Round Table", "Knights of Camelot", 100, "NILHUIONIT132"),
+            new Book("How to get a Million Subscribers", "HatFilms", 250, "LKJHGFDSAQ524")
+        };
+
+        // GET: api/Book
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return data;
+        }
+
+        // GET: api/Book/5
+        [HttpGet("{id}", Name = "Get")]
+        public Book Get(string id)
+        {
+            return data.Find(b => b.Isbn == id);
+        }
+
+        // POST: api/Book
+        [HttpPost]
+        public void Post([FromBody] Book value)
+        {
+            data.Add(value);
+        }
+
+        // PUT: api/Book/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] Book value)
+        {
+            Book book = Get(id);
+            if (book !=null)
+            {
+                book.Isbn = value.Isbn;
+                book.Author = value.Author;
+                book.Pages = value.Pages;
+                book.Title = value.Title;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            Book book = Get(id);
+            if (book != null)
+            {
+                data.Remove(book);
+            }
+        }
+    }
+}
